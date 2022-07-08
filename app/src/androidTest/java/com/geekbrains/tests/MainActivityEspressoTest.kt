9a57077bc6b8ba8fd937db0d06ce8b840de41a29
andlekoal <andlekoal@gmail.com>
2022-07-08 14:47:23 +0300

    Test with flavors setup is finished

--- app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -34,11 +34,11 @@ class MainActivityEspressoTest {
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
 
-        if (BuildConfig.TYPE == MainActivity.FAKE) {
+        if (BuildConfig.FLAVOR == "fake") {
             onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
         } else {
             onView(isRoot()).perform(delay())
-            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2283")))
+            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 3130")))
         }
     }
 
