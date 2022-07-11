74033f4a13153801964117bf33e1758ae9ffea85
andlekoal <andlekoal@gmail.com>
2022-07-11 09:44:41 +0300

    UI Automator implementation added

---
+++ app/src/androidTest/java/com/geekbrains/tests/automator/InitialTest.kt
@@ -0,0 +1,11 @@
+package com.geekbrains.tests.automator
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 18)
+class InitialTest {
+
+}
