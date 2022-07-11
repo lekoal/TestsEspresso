e33aacdbef5b285d0a2d1956de055e244f5601e5
andlekoal <andlekoal@gmail.com>
2022-07-11 10:26:54 +0300

    ImeActionButton press is removed

--- app/src/androidTest/java/com/geekbrains/tests/automator/MainActivityTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/automator/MainActivityTest.kt
@@ -1,20 +1,56 @@
 package com.geekbrains.tests.automator
 
 import android.content.Context
+import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
+import com.geekbrains.tests.test.BuildConfig
+import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @SdkSuppress(minSdkVersion = 18)
 class MainActivityTest {
+    private val uiDevice: UiDevice = UiDevice.getInstance(getInstrumentation())
     private val context = ApplicationProvider.getApplicationContext<Context>()
     private val packageName = context.packageName
 
+    @Before
+    fun setUp() {
+        uiDevice.pressHome()
+        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
+        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        context.startActivity(intent)
+        uiDevice.wait(Until.hasObject(By.pkg(packageName).depth(0)), TIMEOUT)
+    }
+
+    companion object {
+        private const val TIMEOUT = 5000L
+    }
+
+    @Test
+    fun mainActivity_IsStarted() {
+        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
+        assertNotNull(editText)
+    }
+
     @Test
     fun search_IsWorking() {
+        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
+        val button = uiDevice.findObject(By.res(packageName, "searchButton"))
+        editText.text = ""
+        button.click()
+        if (BuildConfig.FLAVOR == "fake") {
 
+        } else {
+
+        }
     }
 }
