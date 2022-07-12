4a117914baf5411d90d397bd48f17a87a7b3b1fb
andlekoal <andlekoal@gmail.com>
2022-07-12 10:41:35 +0300

    Recorded Espresso test cleared

--- app/src/androidTest/java/com/geekbrains/tests/view/search/RecordedEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/view/search/RecordedEspressoTest.kt
@@ -1,8 +1,6 @@
 package com.geekbrains.tests.view.search
 
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -11,10 +9,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.geekbrains.tests.R
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,13 +27,6 @@ class RecordedEspressoTest {
         val appCompatEditText = onView(
             allOf(
                 withId(R.id.searchEditText),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    1
-                ),
                 isDisplayed()
             )
         )
@@ -47,13 +35,6 @@ class RecordedEspressoTest {
         val appCompatEditText2 = onView(
             allOf(
                 withId(R.id.searchEditText),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    1
-                ),
                 isDisplayed()
             )
         )
@@ -62,13 +43,6 @@ class RecordedEspressoTest {
         val materialButton = onView(
             allOf(
                 withId(R.id.searchButton), withText("search"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
                 isDisplayed()
             )
         )
@@ -77,13 +51,6 @@ class RecordedEspressoTest {
         val materialButton2 = onView(
             allOf(
                 withId(R.id.toDetailsActivityButton), withText("to details"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    3
-                ),
                 isDisplayed()
             )
         )
@@ -92,13 +59,6 @@ class RecordedEspressoTest {
         val materialButton3 = onView(
             allOf(
                 withId(R.id.incrementButton), withText("+"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
                 isDisplayed()
             )
         )
@@ -107,28 +67,9 @@ class RecordedEspressoTest {
         val textView = onView(
             allOf(
                 withId(R.id.totalCountTextView), withText("Number of results: 43"),
-                withParent(withParent(withId(android.R.id.content))),
                 isDisplayed()
             )
         )
         textView.check(matches(withText("Number of results: 43")))
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
